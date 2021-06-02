821db07d6c4cd3fb2a3f4d05c7d14e2a98ba639b
zangguojun <1739932260@qq.com>
2021-06-02 12:50:28 +0800

    22/06/02

---
+++ 09-闭包考核题.js
@@ -0,0 +1,53 @@
+let x = 5
+function fn(x) {
+  return function (y) {
+    console.log(y + (++x));
+  }
+}
+let f = fn(6)
+f(7)            // 14
+fn(8)(9)        // 18
+f(10)           // 18
+console.log(x); // 5
+
+console.log('--------------------');
+
+var i = 0
+var j = 5
+console.log(j + ++i, i);
+var i = 0
+var j = 5
+console.log(j + (++i), i);
+var i = 0
+var j = 5
+console.log(j + i++, i);
+var i = 0
+var j = 5
+console.log(j + (i++), i);
+
+console.log('--------------------');
+
+let a = 0, b = 0
+function A(a) {
+  A = function (b) {
+    console.log(a + b ++);
+  }
+  console.log(a ++);
+}
+A(1) // 1
+A(2) // 4
+
+console.log('--------------------');
+
+let aa = bb = 0
+let aaa = 0, bbb = 0
+console.log(aa, bb);
+console.log(aaa, bbb);
+
+console.log('--------------------');
+
+let obj = {
+  name: 'buchiyu'
+}
+// alert(obj)        // { name: 'buchiyu' }
+console.log(obj)  // [object Object]
